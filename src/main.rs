2459bde75c04bb0f6f3a921a9463fdbeeea15cfb
Ryan O'Beirne <ryanobeirne@gmail.com>
2018-07-11 13:32:38 -0400

    fix exponent to powi(2)

--- src/main.rs
+++ src/main.rs
@@ -1,32 +1,55 @@
 
 #[derive(Debug)]
 struct LabValue {
-    L: f64,
+    l: f64,
     a: f64,
     b: f64,
 }
 
-fn delta_e_1976(c1: LabValue, c2: LabValue) -> f64 {
-    ( (c1.L - c2.L).exp2() + (c1.a - c2.a).exp2() + (c1.b - c2.b).exp2() ).sqrt()
+#[derive(Debug)]
+struct LchValue {
+    l: f64,
+    c: f64,
+    h: f64,
+}
+
+fn lab_to_lch(color: &LabValue) -> LchValue {
+    LchValue {
+        l: color.l,
+        c: ( color.a.powi(2) + color.b.powi(2) ).sqrt(),
+        h: color.b,        
+    }
+}
+
+fn delta_e_1976(c1: &LabValue, c2: &LabValue) -> f64 {
+    ( (c1.l - c2.l).powi(2) + (c1.a - c2.a).powi(2) + (c1.b - c2.b).powi(2) ).sqrt()
 }
 
 fn main() {
 
     let color0 = LabValue {
-        L: 95.08,
+        l: 95.08,
         a: -0.17,
         b: -10.81,
     };
 
     let color1 = LabValue {
-        L: 89.73,
+        l: 89.73,
         a: 1.88,
         b: -6.96,
     };
 
-    let delta_e = delta_e_1976(color0, color1);
+    let color2 = LabValue {
+        l: 50.0,
+        a: 100.0,
+        b: 100.0,
+    };
 
+    let delta_e = delta_e_1976(&color0, &color1);
     println!("{:?}", delta_e);
+
+    let lch = lab_to_lch(&color2);
+    println!("{:?}", lch);
 
 }
 
