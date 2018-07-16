775509521aee576387b1027ab258c85c44afd42f
Ryan O'Beirne <ryanobeirne@gmail.com>
2018-07-15 23:52:21 -0400

    I think de2000 really works now

--- src/main.rs
+++ src/main.rs
@@ -15,12 +15,11 @@ struct LchValue {
     h: f64,
 }
 
-//Not in use yet, but will be necessary for de2000
 fn lab_to_lch(color: &LabValue) -> LchValue {
     let mut h: f64 = color.b.atan2(color.a).to_degrees();
 
     if h < 0.0 {
-        h = 360.0 - h.abs();
+        h += 360.0;
     };
 
     LchValue {
@@ -56,37 +55,71 @@ fn delta_e_1976(c0: &LabValue, c1: &LabValue) -> f64 {
 }
 
 fn delta_e_2000(c0: &LabValue, c1:&LabValue) -> f64 {
-    let lch_0 = lab_to_lch(&c0);
-    let lch_1 = lab_to_lch(&c1);
-    let l_avg = (lch_0.l + lch_1.l) / 2.0;
-    let c_avg = (lch_0.c + lch_1.c) / 2.0;
-    let h_avg = avg_h_calc(&lch_0.h, &lch_1.h);
-    let t = 1.0 - 0.17*((    h_avg - 30.0)).to_radians().cos()
-                + 0.24*((2.0*h_avg         )).to_radians().cos()
-                + 0.32*((3.0*h_avg +  6.0)).to_radians().cos()
-                - 0.20*((4.0*h_avg - 63.0)).to_radians().cos();
+    let l_bar_prime = (c0.l + c1.l)/2.0;
+    let c_0 = (c0.a.powi(2) + c0.b.powi(2)).sqrt();
+    let c_1 = (c1.a.powi(2) + c1.b.powi(2)).sqrt();
+    let c_bar = (c_0 + c_1) / 2.0;
+    let g = 0.5 * (1.0 - ( c_bar.powi(7) / (c_bar.powi(7) + 25_f64.powi(7)) ).sqrt());
+    let a_prime_0 = c0.a * (1.0 + g);
+    let a_prime_1 = c1.a * (1.0 + g);
+    let c_prime_0 = (a_prime_0.powi(2) + c0.b.powi(2)).sqrt();
+    let c_prime_1 = (a_prime_1.powi(2) + c1.b.powi(2)).sqrt();
+    let c_bar_prime = (c_prime_0 + c_prime_1) / 2.0;
+    
+    let mut h_prime_0 = c0.b.atan2(a_prime_0).to_degrees();
+    if h_prime_0 < 0.0 {
+        h_prime_0 += 360.0;
+    };
+
+    let mut h_prime_1 = c1.b.atan2(a_prime_1).to_degrees();
+    if h_prime_1 < 0.0 {
+        h_prime_1 += 360.0;
+    };
+
+    let mut h_bar_prime = h_prime_0 - h_prime_1;
+    if h_bar_prime > 180.0 {
+        h_bar_prime = (h_prime_0 + h_prime_1 + 360.0) / 2.0;
+    } else {
+        h_bar_prime = (h_prime_0 + h_prime_1) / 2.0;
+    };
+
+    let t = 1.0 - 0.17*(    h_bar_prime - 30.0).to_radians().cos()
+                + 0.24*(2.0*h_bar_prime       ).to_radians().cos()
+                + 0.32*(3.0*h_bar_prime +  6.0).to_radians().cos()
+                - 0.20*(4.0*h_bar_prime - 63.0).to_radians().cos();
+    
+    let mut delta_h = (h_prime_1 - h_prime_0).abs();
+    if delta_h > 180.0 && h_prime_1 <= h_prime_0 {
+        delta_h += 360.0;
+    } else if delta_h > 180.0 {
+        delta_h -= 360.0;
+    };
+
+    let delta_l_prime = c1.l - c0.l;
+    let delta_c_prime = c_prime_1 - c_prime_0;
+    let delta_h_prime = 2.0 * (c_prime_0 * c_prime_1).sqrt() * (delta_h.to_radians() / 2.0).sin();
+
     let s_l = 1.0 + (
-                        (0.015 * (l_avg - 50.0).powi(2))
-                      / ( 20.0 + (l_avg - 50.0).powi(2)).sqrt()
-                      );
-    let s_c = 1.0 + 0.045*c_avg;
-    let s_h = 1.0 + 0.015*c_avg*t;
-    let r_t = -2.0*((c_avg.powi(7)/(c_avg.powi(7) + 25_f64.powi(7))).sqrt());
-    let delta_l = lch_1.l - lch_0.l;
-    let delta_c = lch_1.c - lch_0.c;
-    let delta_h = 2.0*(lch_0.c*lch_1.c).sqrt()*(delta_h_calc(&lch_0.h, &lch_1.h)/2.0).sin();
+              (0.015 * (l_bar_prime - 50.0).powi(2))
+            / (20.00 + (l_bar_prime - 50.0).powi(2)).sqrt()
+        );
+    let s_c = 1.0 + 0.045 * c_bar_prime;
+    let s_h = 1.0 + 0.015 * c_bar_prime * t;
+    
+    let delta_theta = 30.0 * (-((h_bar_prime - 275.0)/25.0).powi(2)).exp();
+    let r_c =  2.0 * (c_bar_prime.powi(7)/(c_bar_prime.powi(7) + 25_f64.powi(7))).sqrt();
+    let r_t = -(r_c * (2.0 * delta_theta.to_radians()).sin());
 
-    println!("r_t:\t{}", r_t);
-    println!("t:\t{}", t);
-    println!("s_l:\t{}", s_l);
-    println!("s_c:\t{}", s_c);
-    println!("s_h:\t{}", s_h);
+    let k_l = 1.0;
+    let k_c = 1.0;
+    let k_h = 1.0;
 
-    //Return de2000
-    ( (delta_l/s_l).powi(2)
-    + (delta_c/s_c).powi(2)
-    + (delta_h/s_h).powi(2)
-    + r_t*(delta_c/s_c)*(delta_h/s_h)
+    //Return the Delta E 2000
+    (
+        (delta_l_prime/(k_l*s_l)).powi(2)
+      + (delta_c_prime/(k_c*s_c)).powi(2)
+      + (delta_h_prime/(k_h*s_h)).powi(2)
+      + (r_t * (delta_c_prime/(k_c*s_c)) * (delta_h_prime/(k_h*s_h)))
     ).sqrt()
 }
 
