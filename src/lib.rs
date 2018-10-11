77fd7730e7a1e2102bf39d573238a3f4da765b3a
Ryan O'Beirne <robeirne@spoonflower.com>
2018-10-11 17:21:28 -0400

    Add further tests and ValueErrors for string conversion

--- src/lib.rs
+++ src/lib.rs
@@ -1,25 +1,33 @@
-#[derive(Debug)]
+#[derive(Debug, PartialEq)]
 pub struct LabValue {
     pub l: f64,
     pub a: f64,
     pub b: f64,
 }
 
-#[derive(Debug)]
+#[derive(Debug, PartialEq)]
 pub struct LchValue {
     pub l: f64,
     pub c: f64,
     pub h: f64,
 }
 
+#[derive(Debug)]
+pub enum ValueError {
+    OutOfBounds,
+    BadFormat,
+}
+
+pub type ValueResult<T> = Result<T, ValueError>;
+
 impl LabValue {
-    pub fn new() -> LabValue {
+    pub fn zero() -> LabValue {
         //! New LabValue with a value of 0,0,0.
         LabValue { l: 0.0, a: 0.0, b: 0.0 }
     }
 
-    pub fn from(s: &str) -> LabValue {
-       string_to_lab(s).unwrap()
+    pub fn from(s: &str) -> ValueResult<LabValue> {
+       Ok(string_to_lab(s)?)
     }
 
     pub fn to_lch(&self) -> LchValue {
@@ -36,28 +44,75 @@ impl LabValue {
             h: h % 360.0,
         }
     }
+
+    pub fn round_to(&self, places: i32) -> LchValue {
+        LchValue {
+            l: round_to(self.l, places),
+            c: round_to(self.a, places),
+            h: round_to(self.b, places),
+        }       
+    }
 }
 
-use std::fmt::Error;
-pub fn string_to_lab(lab_string: &str) -> Result<LabValue, Error> {
-    //! Convert and validate strings to LabValue.
+impl LchValue {
+    pub fn zero() -> LchValue {
+        //! New LchValue with a value of 0,0,0
+        LchValue { l: 0.0, c: 0.0, h: 0.0 }
+    }
+
+    pub fn from(s: &str) -> ValueResult<LchValue> {
+       Ok(string_to_lch(s)?)
+    }
+
+    pub fn to_lab(&self) -> LabValue {
+        LabValue {
+            l: self.l,
+            a: self.c * self.h.to_radians().cos(),
+            b: self.c * self.h.to_radians().sin(),
+        }
+    }
+
+    pub fn round_to(&self, places: i32) -> LchValue {
+        LchValue {
+            l: round_to(self.l, places),
+            c: round_to(self.c, places),
+            h: round_to(self.h, places),
+        }       
+    }
+}
+
+pub fn round_to(val: f64, places: i32) -> f64 {
+    let mult = 10_f64.powi(places);
+    (val * mult).round() / mult
+}
+
+pub fn string_to_lab(lab_string: &str) -> ValueResult<LabValue> {
+    //! Validate and convert strings to LabValue.
     //! Split string by comma (92.5,33.5,-18.8).
-    let split = lab_string.split(",").filter_map(|s| s.parse::<f64>().ok()).collect::<Vec<_>>();
+    let s = lab_string.split(",");
+    let st: Vec<&str> = s.clone().collect();
+
     //Validate that there are only 3 values
-    if split.len() != 3 {
-        eprintln!("Bad Lab values format: '{}'.\n\tUse 'L,a,b'", lab_string);
-        return Err(Error);
+    if st.len() != 3 {
+        return Err(ValueError::BadFormat);
     };
+
+    let split = s.filter_map(|s| s.parse::<f64>().ok()).collect::<Vec<f64>>();
+
+    //Validate that there are only 3 floats
+    if split.len() != 3 {
+        // eprintln!("Bad Lab values format: '{}'\n\tUse 'L,a,b'", lab_string);
+        return Err(ValueError::BadFormat);
+    };
+
     //Check that the Lab values are in the proper range or Error
     if  split[0] < 0.0    || split[0] > 100.0 ||
         split[1] < -128.0 || split[1] > 128.0 ||
-        split[2] < -128.0 || split[2] > 128.0 {
-            eprintln!("Bad Lab values: {}", lab_string);
-            eprintln!("\tL: 0..100\n\ta: -128..128\n\tb: -128..128");
-            return Err(Error);
+        split[2] < -128.0 || split[2] > 128.0
+    {
+        return Err(ValueError::OutOfBounds);
     };
 
-    //Return the LabValue
     let lab = LabValue {
         l: split[0],
         a: split[1],
@@ -65,6 +120,40 @@ pub fn string_to_lab(lab_string: &str) -> Result<LabValue, Error> {
     };
 
     Ok(lab)
+}
+
+pub fn string_to_lch(lch_string: &str) -> ValueResult<LchValue> {
+    //! Validate and convert strings to LchValue.
+    //! Split string by comma (92.5,33.5,240.3).
+    let s = lch_string.split(",");
+    let st: Vec<&str> = s.clone().collect();
+
+    // Validate that there are only 3 values
+    if st.len() != 3 {
+        return Err(ValueError::BadFormat);
+    };
+
+    let split = s.filter_map(|s| s.parse::<f64>().ok()).collect::<Vec<f64>>();
+
+    // Validate that there are only 3 floats
+    if split.len() != 3 {
+        return Err(ValueError::BadFormat);
+    };
+    // Check that the Lab values are in the proper range or Error
+    if  split[0] < 0.0 || split[0] > 100.0 ||
+        split[1] < 0.0 || split[1] > (128_f64.powi(2) + 128_f64.powi(2)).sqrt() ||
+        split[2] < 0.0 || split[2] > 360.0
+    {
+        return Err(ValueError::OutOfBounds);
+    };
+
+    let lch = LchValue {
+        l: split[0],
+        c: split[1],
+        h: split[2],
+    };
+
+    Ok(lch)
 }
 
 pub fn delta_e_1976(c0: &LabValue, c1: &LabValue) -> f64 {
