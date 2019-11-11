b593e522c64231f5fb4e2c5f37b2689d2e9a8630
Ryan O'Beirne <robeirne@spoonflower.com>
2019-11-11 12:06:25 -0500

    Comments

--- examples/readme.rs
+++ examples/readme.rs
@@ -21,6 +21,7 @@ fn main() -> Result<(), Box<dyn Error>>{
     // Convert to other color types
     let lch0 = LchValue::from(lab0);
     let xyz0 = XyzValue::from(lab1);
+    // If DE2000 is less than 1.0, the colors are considered equivalent
     assert_eq!(lch0, lab0);
     assert_eq!(xyz0, lab1);
 
@@ -31,15 +32,14 @@ fn main() -> Result<(), Box<dyn Error>>{
     // Usually rounding to 4 decimal places is more than enough.
 
     println!("{}\n{}\n{}\n{}\n{}\n{}\n{}\n",
-        lab0, lab1, lch0, xyz0, de0, de1, de2,
+        lab0, // [L:89.73, a:1.88, b:-6.96]
+        lab1, // [L:95.08, a:-0.17, b:-10.81]
+        lch0, // [L:89.73, c:7.2094383, h:285.11572]
+        xyz0, // [X:0.84574246, Y:0.8780792, Z:0.8542397]
+        de0,  // 5.316941
+        de1,  // 5.316941
+        de2,  // 5.316937
     );
-    // [L:89.73, a:1.88, b:-6.96]
-    // [L:95.08, a:-0.17, b:-10.81]
-    // [L:89.73, c:7.2094383, h:285.11572]
-    // [X:0.8336992, Y:0.8780792, Z:1.1272838]
-    // 5.316941
-    // 5.316941
-    // 5.316937
 
     Ok(())
 }
--- src/lib.rs
+++ src/lib.rs
@@ -4,26 +4,47 @@
 //! # Examples
 //!
 //! ```
-//! use deltae::*;
-//! use std::str::FromStr;
 //! use std::error::Error;
-//! use std::convert::TryFrom;
+//! use std::str::FromStr;
+//! use deltae::*;
 //!
-//! fn main() -> Result<(), Box<dyn Error>> {
+//! fn main() -> Result<(), Box<dyn Error>>{
+//!     // Lab from a string
 //!     let lab0 = LabValue::from_str("89.73, 1.88, -6.96")?;
+//!     // Lab directly from values
 //!     let lab1 = LabValue {
 //!         l: 95.08,
 //!         a: -0.17,
 //!         b: -10.81,
-//!     }.validate()?;
-//!     let lab2 = LabValue::try_from(&[89.73, 1.88, -6.96])?;
-//!     assert_eq!(lab0, lab2);
+//!     }.validate()?; // Validate that the values are in range
 //!
-//!     println!("{}", &lab0); // [L:89.73, a:1.88, b:-6.96]
-//!
+//!     // Calculate DeltaE between two lab values
 //!     let de0 = DeltaE::new(&lab0, &lab1, DE2000);
-//!     let de1 = lab0.delta(&lab1, DE2000);
-//!     assert_eq!(de0, de1); // DE2000: 5.3169
+//!     // Use the Delta trait
+//!     let de1 = lab0.delta(lab1, DE2000);
+//!     assert_eq!(de0, de1);
+//!
+//!     // Convert to other color types
+//!     let lch0 = LchValue::from(lab0);
+//!     let xyz0 = XyzValue::from(lab1);
+//!     assert_eq!(lch0, lab0);
+//!     assert_eq!(xyz0, lab1);
+//!
+//!     // Calculate DeltaE between different color types
+//!     let de2 = lch0.delta(xyz0, DE2000);
+//!     assert_eq!(de2.round_to(4), de0.round_to(4));
+//!     // There is some loss of accuracy in the conversion.
+//!     // Usually rounding to 4 decimal places is more than enough.
+//!
+//!     println!("{}\n{}\n{}\n{}\n{}\n{}\n{}\n",
+//!         lab0, // [L:89.73, a:1.88, b:-6.96]
+//!         lab1, // [L:95.08, a:-0.17, b:-10.81]
+//!         lch0, // [L:89.73, c:7.2094383, h:285.11572]
+//!         xyz0, // [X:0.84574246, Y:0.8780792, Z:0.8542397]
+//!         de0,  // 5.316941
+//!         de1,  // 5.316941
+//!         de2,  // 5.316937
+//!     );
 //!
 //!     Ok(())
 //! }
